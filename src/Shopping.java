1f393ff6ad756488e4281ca08c7bba6633897e72
tech <asd@sasdasdas.ru>
2024-04-05 21:06:52 +0300

    add test data, update Show_list, add_product_list

--- src/Shopping.java
+++ src/Shopping.java
@@ -2,43 +2,64 @@ import java.util.Scanner;
 
 public class Shopping {
     static Scanner scanner = new Scanner(System.in);
+    static int productCount = 0;
 
     public static void main(String[] args) {
+        System.out.println("Вас приветствует список покупок!");
+        int max_number_products = 8;
+        String[] shoppingList = new String[max_number_products];
 
-
-        String[] shoppingList = new String[8];
-
+        String[] testShoppingList = {"хлеб", "сливочноемасло", "сыр", "докторскаяколбаса"};  //убрать
+        productCount = 4; // убрать
         while (true) {
-            select_command(scanner);
+            testShoppingList = check_select_command(get_select_command(scanner), testShoppingList);
+
         }
     }
 
-    public static void select_command(Scanner scanner) {
+    public static int get_select_command(Scanner scanner) {
         System.out.println("Выберите одну из команд:");
         System.out.println("1. Добавить товар в список");
         System.out.println("2. Показать список");
         System.out.println("3. Очистить список");
         System.out.println("4. Завершить работу");
 
-        int actionNumber = scanner.nextInt();
+        return scanner.nextInt();
 
+    }
+
+    public static String[] check_select_command(int actionNumber, String[] shoppingList) {
         switch (actionNumber) {
             case 1:
-                add_product_list();
+                add_product_list(shoppingList);
+                break;
             case 2:
-                Show_list();
+                Show_list(shoppingList);
+                break;
             case 3:
                 clear_list();
+                break;
             case 4:
                 finish_work();
-
+                break;
+            default:
+                System.out.println("Неизвестная команда!");
+                break;
         }
+        return shoppingList;
     }
 
-    public static void add_product_list() {
+    public static String[] add_product_list(String[] shoppingList) {
+        String productName = scanner.next();
+        shoppingList[productCount] = productName;
+        productCount++;
+        return shoppingList;
     }
 
-    public static void Show_list() {
+    public static void Show_list(String[] shoppingList) {
+        for (int i = 0; i < productCount; i++) {
+            System.out.println("Продукт " + (i + 1) + ":" + shoppingList[i]);
+        }
     }
 
     public static void clear_list() {
