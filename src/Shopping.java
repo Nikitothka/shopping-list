cc0fcd033da8b25fa72eee354f415810a3e9e9c7
tech <asd@sasdasdas.ru>
2024-04-05 20:13:42 +0300

    add method select_command

--- src/Shopping.java
+++ src/Shopping.java
@@ -1,7 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Shopping {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
+
+        String[] shoppingList = new String[8];
+
+        while (true) {
+            select_command(scanner);
+        }
+    }
+    public static void select_command(Scanner scanner) {
+        System.out.println("Выберите одну из команд:");
+        System.out.println("1. Добавить товар в список");
+        System.out.println("2. Показать список");
+        System.out.println("3. Очистить список");
+        System.out.println("4. Завершить работу");
+
+        int actionNumber = scanner.nextInt();
     }
 }
